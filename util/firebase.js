88df15d46122456bd1cb83ab7d5e7226ea9b0005
tiodio324 <nikita324smirnovvv@gmail.com>
2024-10-15 22:57:34 +0300

    Update firebase.js

--- util/firebase.js
+++ util/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/compat/database';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDGGZI3m5WOa1PTcQq_FhUK0v0VW1aaLbo",
-    authDomain: "clip-reader.firebaseapp.com",
-    databaseURL: "https://clip-reader-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: ********************************,
+    authDomain: ********************************,
+    databaseURL: ********************************,
     projectId: "clip-reader",
-    storageBucket: "clip-reader.appspot.com",
-    messagingSenderId: "1516126846",
-    appId: "1:1516126846:web:cad9527b2d0676a13c8e2f",
-    measurementId: "G-2ZF003M08C"
+    storageBucket: ********************************,
+    messagingSenderId: ********************************,
+    appId: ********************************,
+    measurementId: ********************************
 };
 
 if (!firebase.apps.length) {
